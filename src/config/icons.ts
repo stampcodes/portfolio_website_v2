fe4d616aecce7ec636659ec3a2d6138af06d41f9
Andre <andreafragnelli@gmail.com>
2024-10-27 20:38:59 +0100

    Added config icons

---
+++ src/config/icons.ts
@@ -0,0 +1,4 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faLinkedin, faSquareGithub);
