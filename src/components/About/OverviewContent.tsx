0b371ba5a7e0f8a4788e2d8fd9c2e59320cd9169
Andre <andreafragnelli@gmail.com>
2024-10-28 11:07:58 +0100

    Finished overview section

--- src/components/About/OverviewContent.tsx
+++ src/components/About/OverviewContent.tsx
@@ -1,28 +1,37 @@
 const OverviewContent = () => {
   return (
     <div className="bg-[#a5aabf] text-[#1a1f36] p-8 max-w-4xl rounded-xl shadow-lg mt-10">
-      <p className="paragraph mb-4">
-        💻 Passionate about technology and innovation, always seeking new
-        challenges and opportunities for growth.
-      </p>
-      <p className="paragraph mb-4">
-        🌐 Immersed in the vast digital world, discovering that curiosity leads
-        to great satisfaction.
-      </p>
-      <p className="paragraph mb-4">
-        💼 Started working after high school to gain economic independence, then
-        realized the value of passion.
-      </p>
-      <p className="paragraph mb-4">
-        🎓 Took the leap into further studies, now constantly finding
-        satisfaction and moving closer to my professional goals.
-      </p>
-      <p className="paragraph mb-4">
-        💡 Currently deepening my knowledge in web development and blockchain,
-        with a strong interest in emerging technologies and innovative
-        solutions. Aiming to become a Blockchain Developer, contributing to
-        projects that leverage decentralized technology.
-      </p>
+      <div className="paragraph mb-3">
+        <h3 className="font-bold mb-1">🌍 Blockchain & Web Developer Junior</h3>
+        <p>
+          Driven by a passion for technology and innovation, I am eager to
+          tackle new challenges and contribute to cutting-edge solutions in
+          emerging technologies.
+        </p>
+      </div>
+      <div className="paragraph mb-3">
+        <h3 className="font-bold mb-1">💼 Career Path</h3>
+        <p>
+          After finishing school, I chose to work to gain independence and
+          quickly discovered the importance of passion for both professional and
+          personal growth.
+        </p>
+      </div>
+      <div className="paragraph mb-3">
+        <h3 className="font-bold mb-1">🎓 Continuous Learning</h3>
+        <p>
+          I have pursued advanced studies, finding great satisfaction in
+          continuously developing my technical and digital skills.
+        </p>
+      </div>
+      <div className="paragraph">
+        <h3 className="font-bold mb-1">💡 Goal</h3>
+        <p>
+          My goal is to become an impactful Blockchain & Web Developer, adding
+          value to innovative projects in the field of decentralized
+          technologies.
+        </p>
+      </div>
     </div>
   );
 };
