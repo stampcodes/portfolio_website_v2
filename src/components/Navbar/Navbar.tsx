fa7fabdea34db6f6fd94176f6e159cbb6c203c8a
Andre <andreafragnelli@gmail.com>
2024-10-29 18:32:43 +0100

    feat: add link to logo in Navbar to navigate back to homepage

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,9 @@ import SocialIcons from "./SocialIcons";
 const Navbar: React.FC = () => {
   return (
     <nav className="fixed top-0 left-0 w-full flex justify-center bg-[#1a1f36] md:bg-transparent items-center z-10 p-4 lg:justify-around">
-      <img className="w-[200px] p-2" src={logo} alt="logo" />
+      <a href="https://andreafragnelli.vercel.app/" rel="noopener noreferrer">
+        <img className="pointer w-[200px] p-2" src={logo} alt="logo" />
+      </a>
       <SocialIcons />
     </nav>
   );
